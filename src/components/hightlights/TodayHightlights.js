acf21e30aa60d8f8852e794c86ca47753630da03
JamesHow1ett <mopis101@gmail.com>
2020-10-15 20:06:38 +0300

    feat(today's hightlights): layout block, show data from props
    Signed-off-by: JamesHow1ett <mopis101@gmail.com>

--- src/components/hightlights/TodayHightlights.js
+++ src/components/hightlights/TodayHightlights.js
@@ -1,19 +1,72 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+//styles
+import './TodayHightlights.scss';
+
 export default class TodayHightlights extends Component {
-  static propTypes = {
-    prop: PropTypes
-  }
 
   render() {
     return (
-      <div>TodayHightlights!!!
-        <div className="wind-status"></div>
-        <div className="humidity"></div>
-        <div className="visibility"></div>
-        <div className="air-pressure"></div>
+      <div className="today-hightlights">
+        <div className="today-hightlights__header">
+          <span>Today's Hightlights</span>
+        </div>
+        <div className="today-hightlights__wrapper">
+          <div className="today-hightlights__box">
+            <div className="today-hightlights__item">
+              <div className="today-hightlights__label">
+                <span>Wind status</span>
+              </div>
+              <div className="today-hightlights__info">
+                <span>{Number(this.props.dataApi.data[0]['wind_speed']).toFixed(1)}<span className="today-hightlights__info_small">&nbsp;mph</span></span>
+              </div>
+              <div className="today-hightlights__wind-direction">
+                <img src="" alt=""></img>
+                <span>{this.props.dataApi.data[0]['wind_direction_compass']}</span>
+              </div>
+            </div>
+          </div>
+          <div className="today-hightlights__box">
+            <div className="today-hightlights__item">
+              <div className="today-hightlights__label">
+                <span>Humidity</span>
+              </div>
+              <div className="today-hightlights__info">
+                <span>{this.props.dataApi.data[0]['humidity']}%</span>
+              </div>
+              <div className="today-hightlights__progress-bar">
+                <progress className="progress-bar" max="100" value={this.props.dataApi.data[0]['humidity']}></progress>
+              </div>
+            </div>
+          </div>
+          <div className="today-hightlights__box today-hightlights__box_small">
+            <div className="today-hightlights__item">
+              <div className="today-hightlights__label">
+                <span>Visibility</span>
+              </div>
+              <div className="today-hightlights__info">
+                <span>{Number(this.props.dataApi.data[0]['visibility']).toFixed(1)}<span className="today-hightlights__info_small">&nbsp;miles</span></span>
+              </div>
+            </div>
+          </div>
+          <div className="today-hightlights__box today-hightlights__box_small">
+            <div className="today-hightlights__item">
+              <div className="today-hightlights__label">
+                <span>Air Pressure</span>
+              </div>
+              <div className="today-hightlights__info">
+                <span>{Number(this.props.dataApi.data[0]['air_pressure']).toFixed(0)}<span className="today-hightlights__info_small">&nbsp;mb</span></span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="today-hightlights__dev-sign"></div>
       </div>
     )
   }
+}
+
+TodayHightlights.propTypes = {
+  dataApi: PropTypes.object.isRequired
 }
