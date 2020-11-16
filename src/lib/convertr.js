2391e0b605b80440d8c7264f89edc2513eb19174
JamesHow1ett <mopis101@gmail.com>
2020-11-17 00:28:02 +0200

    fix(convertr): if to fn passed number(0)
    Signed-off-by: JamesHow1ett <mopis101@gmail.com>

--- src/lib/convertr.js
+++ src/lib/convertr.js
@@ -1,14 +1,14 @@
 //my convertr scales file
 function toMiles (kilometrs) {
-  return Number((kilometrs * 0.6214).toFixed(1));
+  return kilometrs === 0 ? 0 : Number((kilometrs * 0.6214).toFixed(1));
 }
 
 function toKilometrs (miles) {
-  return Number((miles * 1.6).toFixed(1));
+  return miles === 0 ? 0 : Number((miles * 1.6).toFixed(1));
 }
 
 function toFahrenheit (celsius) {
-  return (celsius * 9 / 5) + 32;
+  return celsius === 0 ? 0 : (celsius * 9 / 5) + 32;
 }
 
 
