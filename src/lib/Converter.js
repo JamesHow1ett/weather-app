f07ac79659cd837a64b16d8d59e1fe44844a6b3a
JamesHow1ett <mopis101@gmail.com>
2020-11-20 23:55:29 +0200

    feat(converter): add new methods
    Signed-off-by: JamesHow1ett <mopis101@gmail.com>

--- src/lib/Converter.js
+++ src/lib/Converter.js
@@ -1,19 +1,48 @@
-//my Converter scales class, will extend in future
+/*
+  my Converter scales class, will extend in future
+  class get a two params: first is scale what need to convert,
+  secont is how math need left simbols after point
+*/
 class Converter {
-  constructor(value) {
+  constructor(value, fixedRangeValue) {
     this.value = value
+    this.fixedRangeValue = fixedRangeValue
   }
 
-  toMilesFromKilometrs () {
-    return this.value < 0 ? 0 : Number((this.value * 0.6214).toFixed(1))
+  milesToKilometrs() {
+    return this.value < 0 ? 0 : Number((this.value * 0.6214).toFixed(this.fixedRangeValue))
   }
 
-  toKilometrsFromMiles () {
-    return this.value < 0 ? 0 : Number((this.value * 1.6).toFixed(1))
+  kilometrsToMiles() {
+    return this.value < 0 ? 0 : Number((this.value * 1.6).toFixed(this.fixedRangeValue))
   }
 
-  toFahrenheitFromCelsius () {
-    return Number(((this.value * 9 / 5) + 32).toFixed(0))
+  fahrenheitToCelsius() {
+    return Number(((this.value * 9 / 5) + 32).toFixed(this.fixedRangeValue))
+  }
+
+  inchesToCentimeters() {
+    return Number((this.value * (2.54 / this.value)).toFixed(this.fixedRangeValue))
+  }
+
+  centimetersToInches() {
+    return Number((this.value * (1 / 2.54)).toFixed(this.fixedRangeValue))
+  }
+
+  feetToMeters() {
+    return Number((this.value / 3.2808).toFixed(this.fixedRangeValue))
+  }
+
+  metersToFeet() {
+    return Number((this.value * 3.2808).toFixed(this.fixedRangeValue))
+  }
+
+  poundsToKilograms() {
+    return Number((this.value / 2.2046).toFixed(this.fixedRangeValue))
+  }
+
+  kilogramsToPounds() {
+    return Number((this.value * 2.2046).toFixed(this.fixedRangeValue))
   }
 }
 
